d65bdaaddc61fc8f650eef9aa76cf80883f7512b
Anton Patsev <patsev.anton@gmail.com>
2024-07-30 21:23:19 +0600

    добавил output, network

--- examples/one-port/main.tf
+++ examples/one-port/main.tf
@@ -1,8 +1,29 @@
+data "yandex_client_config" "client" {}
+
+module "network" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-vpc.git?ref=v1.0.0"
+
+  folder_id = data.yandex_client_config.client.folder_id
+
+  blank_name = "vpc-nat-gateway"
+  labels = {
+    repo = "terraform-yacloud-modules/terraform-yandex-vpc"
+  }
+
+  azs = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
+
+  private_subnets = [["10.4.0.0/24"]]
+
+  create_vpc         = true
+  create_nat_gateway = true
+}
+
+
 module "sg" {
   source = "../../"
 
-  folder_id   = "xxx"
-  vpc_id      = "xxx"
+  folder_id   = data.yandex_client_config.client.folder_id
+  vpc_id      = module.network.vpc_id
   blank_name  = "your-security-group-name"
   description = "Your security group description"
   labels = {
